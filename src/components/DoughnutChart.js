e345002dda48c28cf910084ba9eae676cc21e871
Arun Raj Javagal Lakkashetty <55321678+A00435847@users.noreply.github.com>
2020-07-15 01:41:33 -0400

    Add files via upload
    added extra pie chart

---
+++ src/components/DoughnutChart.js
@@ -0,0 +1,138 @@
+import React, { PureComponent } from "react";
+import { PieChart, Pie, Sector } from "recharts";
+
+const animatePie = (props) => {
+  const RADIAN = Math.PI / 180;
+  const {
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    startAngle,
+    endAngle,
+    fill,
+    payload,
+    percent,
+    value,
+  } = props;
+  const sin = Math.sin(-RADIAN * midAngle);
+  const cos = Math.cos(-RADIAN * midAngle);
+  const sx = cx + (outerRadius + 10) * cos;
+  const sy = cy + (outerRadius + 10) * sin;
+  const mx = cx + (outerRadius + 30) * cos;
+  const my = cy + (outerRadius + 30) * sin;
+  const ex = mx + (cos >= 0 ? 1 : -1) * 22;
+  const ey = my;
+  const textAnchor = cos >= 0 ? "start" : "end";
+
+  return (
+    <g>
+      <text x={cx} y={cy} dy={8} textAnchor="middle" fill={fill}>
+        {payload.label}
+      </text>
+      <Sector
+        cx={cx}
+        cy={cy}
+        innerRadius={innerRadius}
+        outerRadius={outerRadius}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        fill={fill}
+      />
+      <Sector
+        cx={cx}
+        cy={cy}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        innerRadius={outerRadius + 6}
+        outerRadius={outerRadius + 10}
+        fill={fill}
+      />
+      <path
+        d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`}
+        stroke={fill}
+        fill="none"
+      />
+      <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
+      <text
+        x={ex + (cos >= 0 ? 1 : -1) * 12}
+        y={ey}
+        textAnchor={textAnchor}
+        fill="#333"
+      >{`Count ${value}`}</text>
+      <text
+        x={ex + (cos >= 0 ? 1 : -1) * 12}
+        y={ey}
+        dy={18}
+        textAnchor={textAnchor}
+        fill="#999"
+      >
+        {`(Rate ${(percent * 100).toFixed(2)}%)`}
+      </text>
+    </g>
+  );
+};
+
+export default class DoughnutChart extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeIndex: 0,
+    };
+  }
+
+  onPieEnter = (data, index) => {
+    this.setState({
+      activeIndex: index,
+    });
+  };
+
+  expandedForm = (data) => {
+    let expandedDataArray = [...data];
+    expandedDataArray = expandedDataArray.map((item) => {
+      if (item.label === "F") {
+        return {
+          label: "Female",
+          value: item.value,
+        };
+      } else if (item.label === "M") {
+        return {
+          label: "Male",
+          value: item.value,
+        };
+      } else if (item.label === "N") {
+        return {
+          label: "Non-Binary",
+          value: item.value,
+        };
+      }
+      return item;
+    });
+    return expandedDataArray;
+  };
+
+  render() {
+    let { data } = this.props;
+    data =
+      data.length > 0
+        ? this.expandedForm(data)
+        : [{ label: "Student", value: 100 }];
+    return (
+      <PieChart width={400} height={400}>
+        <Pie
+          activeIndex={this.state.activeIndex}
+          activeShape={animatePie}
+          data={data}
+          cx={200}
+          cy={200}
+          innerRadius={60}
+          outerRadius={80}
+          fill="#8884d8"
+          dataKey="value"
+          onMouseEnter={this.onPieEnter}
+        />
+      </PieChart>
+    );
+  }
+}
