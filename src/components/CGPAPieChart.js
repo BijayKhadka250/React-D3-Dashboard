5e2a915f5dd34ae41c1fcd312bd79a3fdb69e103
Bijay Khadka <55320541+BijayKhadka250@users.noreply.github.com>
2020-07-22 12:39:34 -0300

    two more graphs

---
+++ src/components/CGPAPieChart.js
@@ -0,0 +1,130 @@
+import React, { useEffect, useRef } from "react";
+import * as d3 from "d3";
+import './CGPAPieChart.css'
+
+
+    const size = {
+       
+        innerRadius: 0,
+        outerRadius: 150
+      };
+
+      const CGPAPieChart = props => {
+        // console.log(props.data)
+        const ref = useRef(null);
+        const createPie = d3
+          .pie()
+          .value(d => d.value)
+          .sort(null);
+        const createArc = d3
+          .arc()
+          .innerRadius(size.innerRadius)
+          .outerRadius(size.outerRadius);
+        const colors = d3.scaleOrdinal(d3.schemeCategory10);
+
+        var margin = {top: 50, right: 0, bottom: 50, left: 50};
+        var width = 600 - margin.left - margin.right;
+        var height = 400 - margin.top - margin.bottom;
+        useEffect(() => {
+        ref.current.innerHTML = null;
+
+        var div = d3.select("body").append("div")	
+        .attr("class", "tooltip2")				
+        .style("opacity", 0);
+        // const total = props.data.reduce((a, b) => ({ count: a.count + b.count }));
+        const data = createPie(props.data);
+        const group = d3.select(ref.current)
+          
+          .append("g")
+          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+          const groupWithData = group.selectAll("g.arc").data(data);
+      
+          groupWithData.exit().remove();
+      
+          const groupWithUpdate = groupWithData
+            .enter()
+            .append("g")
+            .attr("class", "arc");
+      
+          const path = groupWithUpdate
+            .append("path")
+            .merge(groupWithData.select("path.arc"))
+            // .style("stroke","white")
+            // .style("stroke-width",)
+            .on("mouseover", function(d) {	
+                // {console.log(d)}	
+                div.transition()		
+                    .duration(200)		
+                    .style("opacity", .9);		
+                div	.html(d.data.group +": " + d.data.value)	
+                    .style("left", (d3.event.pageX) + "px")		
+                    .style("top", (d3.event.pageY - 28) + "px");	
+                })					
+            .on("mouseout", function(d) {		
+                div.transition()		
+                    .duration(500)		
+                    .style("opacity", 0);	
+            });          
+      
+          path
+            .attr("class", "arc")
+            .attr("d", createArc)
+            .attr("fill", (d, i) => colors(i));
+      
+          const text = groupWithUpdate
+            .append("text")
+            .merge(groupWithData.select("text"));
+      
+          text
+            .attr("text-anchor", "middle")
+            .attr("alignment-baseline", "middle")
+            .attr("transform", d => `translate(${createArc.centroid(d)})`)
+            .style("fill", "white")
+            .style("font-size", 20)
+            .text(d => {return `${d.data.group}`});
+      
+        //   group
+        //     .append("text")
+        //     .attr("text-anchor", "middle")
+        //     .style("font-size", 30)
+        //     .text(total.value ? total.count : null);
+
+            // var keys = ['UnderGraduate','Graduate'];
+            // var z = d3.scaleOrdinal()
+            // .range(["#1f77b4", "#ff7f0e"]);
+            // var legend = group.append("g")
+            //             .attr("font-family", "sans-serif")
+            //             .attr("font-size", 10)
+            //             .attr("text-anchor", "end")
+            //             .selectAll("g")
+            //             .data(keys.slice().reverse())
+            //             .enter().append("g")
+            //             .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+            // legend.append("rect")
+            // .attr("x", width - 200)
+            // .attr("width", 19)
+            // .attr("height", 19)
+            // .attr("fill", z);
+
+            // legend.append("text")
+            // .attr("data-html", "true")
+            // .attr("x", width - 206)
+            // .attr("y", 9.5)
+            // .attr("dy", "0.32em")
+            // .text(function(d) { return d; });
+
+        }, [colors, createArc, createPie, props, props.data,width,height,margin]);
+      
+        return (
+          <svg width={width+margin.left+margin.right} height={height+margin.bottom+margin.top}>
+            <g
+              className="PieChartGrad"
+              ref={ref}
+              transform={`translate(${size.outerRadius} ${size.outerRadius})`}
+            />
+          </svg>
+        );
+}
+
+export default CGPAPieChart;
---
+++ src/components/ResidencyBarGraph.js
@@ -0,0 +1,127 @@
+import React, { useEffect, useRef,useState } from "react";
+import * as d3 from "d3";
+
+
+const ResidencyBarGraph = (props) => {
+    console.log(props.data)
+    const ref = useRef(null);
+
+    
+        var margin = {top: 20, right: 0, bottom: 150, left: 60};
+    var width = 600 - margin.left - margin.right;
+    var height = 400 - margin.top - margin.bottom;
+
+    useEffect(() => {
+        ref.current.innerHTML = null;
+    const svg = d3.select(ref.current)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    
+    // Define the div for the tooltip
+    var div = d3.select("body").append("div")	
+    .attr("class", "tooltip")				
+    .style("opacity", 0);
+
+    var z = d3.scaleOrdinal()
+    .range(["#E24712", "#DEE212","#12E255","#12DAE2","#123EE2","#DB12E2"]);
+
+    const x = d3.scaleBand()
+                  .domain(props.data.map(function(d){return d.residency}))
+                  .range([0, width])
+                  .paddingInner(0.05)
+                  .padding(0.3)
+                  .align(0.1);
+
+    const y = d3.scaleLinear()
+    .domain([0, d3.max(props.data,function(d){return d.count})]).nice()
+    .rangeRound([height, 0]);
+
+    svg
+    .selectAll(".bar")
+    .data(props.data)
+
+    
+    
+    .enter().append("rect")
+    // .transition().duration(200)
+    .attr("class", "bar")
+    
+    .attr("x", function(d) { return x(d.residency); })
+    .attr("y", function(d) { return y(d.count); })
+    .attr("height", function(d) { return height- y(d.count) })
+    .attr("width", x.bandwidth())  
+    .style("fill",function(d){return z(d.residency)}) 
+    .on("mouseover", function(d) {		
+        div.transition()		
+            .duration(200)		
+            .style("opacity", .9);		
+        div	.html(d.count)	
+            .style("left", (d3.event.pageX) + "px")		
+            .style("top", (d3.event.pageY - 28) + "px");	
+        })					
+.on("mouseout", function(d) {		
+        div.transition()		
+            .duration(500)		
+            .style("opacity", 0);	
+    });          
+
+//     svg.selectAll("rect")
+//   .transition()
+//   .duration(800)
+//   .attr("y", function(d) { return y(d.count); })
+//   .attr("height", function(d) { return height - y(d.count); })
+//   .style("fill",function(d){return z(d.faculty)})
+//   .delay(function(d,i){console.log(i) ; return(i*100)})
+  
+  
+    
+    
+    
+
+    svg.append("g")
+.attr("class", "axis x")
+.attr("transform", "translate(0," + height + ")")
+.call(d3.axisBottom(x))
+.selectAll("text")
+    .attr("y", 0)
+    .attr("x", 9)
+    .attr("dy", ".50em")
+    .attr("transform", "rotate(90)")
+    .style("text-anchor", "start");
+
+// svg.append("text")             
+//       .attr("transform",
+//             "translate(" + (width/2) + " ," + 
+//                            (height + margin.top + 20) + ")")
+//       .style("text-anchor", "middle")
+//       .text("Faculties");
+
+svg.append("g")
+.attr("class", "axis y")
+.call(d3.axisLeft(y))
+
+svg.append("text")
+.attr("transform", "rotate(-90)")
+.attr("y", 0 - margin.left)
+.attr("x",0 - (height / 2))
+.attr("dy", "1em")
+.style("text-anchor", "middle")
+.text("No. of Students");
+
+    
+},[margin,height,width,props.data])
+    
+
+
+return (
+    <svg width={width+ margin.left + margin.right} height={height + margin.top + margin.bottom} >
+<g
+className = "ResidencyBarGraph"
+ref={ref}
+/>
+</svg>
+    );
+
+}
+
+export default ResidencyBarGraph;
